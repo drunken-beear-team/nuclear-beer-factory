814e1a13441db8e1c1cc850e0c5f5a12168900d7
Gr3yKnigh1 <gr3yknigh1@gmail.com>
2021-10-02 19:33:07 +0300

    Add space rocket

--- nuclear-beer-factory/bodies/Eater.gd
+++ nuclear-beer-factory/bodies/Eater.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 
-func _on_Fuel_tank_body_entered(body: RigidBody2D):
+func _on_Area2D_body_entered(body):
 	var anim_player: AnimationPlayer = body.get_node("AnimationPlayer")
 	anim_player.play("Dissapear")
 	body.mode = RigidBody2D.MODE_KINEMATIC
