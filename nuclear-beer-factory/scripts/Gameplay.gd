5d63779e26e8490ecbdabe16643b0c85023b79c2
Gr3yKnigh1 <gr3yknigh1@gmail.com>
2021-10-03 09:12:24 +0300

    Create assets dir

--- nuclear-beer-factory/scripts/Gameplay.gd
+++ nuclear-beer-factory/scripts/Gameplay.gd
@@ -1,1 +1,14 @@
 extends Node2D
+
+
+export(NodePath) var rocket_path: NodePath
+
+
+var rocket_anim_player: AnimationPlayer
+
+
+#func _ready() -> void:
+#	rocket_anim_player = 
+#
+#func _process(delta: float) -> void:
+#	pass
